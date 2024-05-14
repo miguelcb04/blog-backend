971d634c1d9f8da5df9c921475eb7249948c65c0
Miguel Chacón Barranco <miguelcb127@gmail.com>
2024-05-14 13:39:37 +0200

    prueba api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,11 +1,19 @@
 import { getPosts, getPostsWithCategory,newPostJson } from "@/lib/actions";
 
-export async function GET( request) {
-  const category = request.nextUrl.searchParams.get("category")
+export async function GET(request) {
+  const category = request.nextUrl.searchParams.get("category");
+  const page = request.nextUrl.searchParams.get("page") || 1; // Obtén el número de página de la consulta, o usa 1 por defecto
   
-  let posts = await getPostsWithCategory(category );
-  console.log('api', posts)
-  return Response.json(posts)
+  const postsPerPage = 2; // Número de publicaciones por página
+  const startIndex = (page - 1) * postsPerPage; // Índice de inicio para la página actual
+  const endIndex = startIndex + postsPerPage; // Índice de fin para la página actual
+
+  let posts = await getPostsWithCategory(category); // Obtén todas las publicaciones con la categoría dada
+  
+  // Filtra las publicaciones para obtener solo las de la página actual
+  const currentPagePosts = posts.slice(startIndex, endIndex);
+
+  return Response.json(currentPagePosts);
 }
 
 

688f3e73805999d89b38b6540040fa61e5f000c6
Miguel Chacón Barranco <miguelcb127@gmail.com>
2024-05-27 12:18:36 +0200

    modificacion de actionsApi

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,69 +1,23 @@
-import { PAGE, PER_PAGE } from '@/lib/pagination';
+import { PAGE } from '@/lib/pagination';
+import { getPostsWithCategoryApi, getAllPostsApi } from '@/lib/actionsApi';
 
 export async function GET(request) {
-  const category = request.nextUrl.searchParams.get("category");
-  const page = request.nextUrl.searchParams.get("page") || PAGE; // Obtén el número de página de la consulta, o usa 1 por defecto
+  const url = new URL(request.url);
+  const category = url.searchParams.get("category");
+  const page = parseInt(url.searchParams.get("page")) || PAGE; // Obtén el número de página de la consulta, o usa 1 por defecto
 
   let posts;
   
   if (category) {
     // Si se proporciona un parámetro de categoría, busca posts solo en esa categoría
-    // http://localhost:3000/api/posts?category=desarrollo-web&page=2
     posts = await getPostsWithCategoryApi(category, page);
   } else {
     // Si no se proporciona un parámetro de categoría, obtener todas según la página
-    // http://localhost:3000/api/posts?page=4
-    // http://localhost:3000/api/posts
-     posts = await getAllPostsApi(page);
+    posts = await getAllPostsApi(page);
   }
 
-  return Response.json(posts);
+  return new Response(JSON.stringify(posts), {
+    headers: { 'Content-Type': 'application/json' }
+  });
 }
 
-async function getPostsWithCategoryApi(categoryName, page) {
-  try {
-    // Calcular el índice de inicio y el número de posts a mostrar
-    const startIndex = (page - 1) * PER_PAGE;
-    
-    // Consulta para obtener los posts con la categoría especificada
-    const posts = await prisma.posts.findMany({
-      include: { categories: true },
-      where: {
-        categories: {
-          some: {
-            slug: categoryName
-          }
-        }
-      },
-      skip: startIndex,
-      take: PER_PAGE
-    });
-
-    console.log('Posts:', posts);
-    return posts;
-  } catch (error) {
-    console.error('Error:', error);
-    return null;
-  }
-}
-
-
-async function getAllPostsApi(page) {
-  try {
-    // Calcular el índice de inicio y el número de posts a mostrar
-    const startIndex = (page - 1) * PER_PAGE;
-    
-    // Consulta para obtener todos los posts
-    const posts = await prisma.posts.findMany({
-      include: { categories: true },
-      skip: startIndex,
-      take: PER_PAGE
-    });
-
-    console.log('All Posts:', posts);
-    return posts;
-  } catch (error) {
-    console.error('Error:', error);
-    return null;
-  }
-}
--- src/lib/actionsApi.js
+++ src/lib/actionsApi.js
@@ -1,4 +1,5 @@
 'use server'
+
 import bcrypt from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { signIn, signOut } from '@/auth';
@@ -7,31 +8,69 @@ import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
 
 export async function editPostJson({ postId, author, title, image, post, slug, views }) {
-  const id = Number(postId)
+  const id = Number(postId);
   try {
     const editpost = await prisma.posts.update({
       where: { id },
       data: { author, title, image, post, slug, views },
-    })
-    return editpost
+    });
+    return editpost;
   } catch (error) {
     console.log(error);
   }
-
 }
 
 export async function deletePostJson(postId) {
-  const id = Number(postId)
+  const id = Number(postId);
   try {
-
     const post = await prisma.posts.delete({
-      where: {
-        id,
-      },
-    })
-    return post
+      where: { id },
+    });
+    return post;
   } catch (error) {
     console.log(error);
   }
+}
 
+const PER_PAGE = 1; // Assuming PER_PAGE is 10, replace with the actual value if different
+
+export async function getPostsWithCategoryApi(categoryName, page) {
+  try {
+    const startIndex = (page - 1) * PER_PAGE;
+    const posts = await prisma.posts.findMany({
+      include: { categories: true },
+      where: {
+        categories: {
+          some: {
+            slug: categoryName
+          }
+        }
+      },
+      skip: startIndex,
+      take: PER_PAGE
+    });
+
+    console.log('Posts:', posts);
+    return posts;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
+
+export async function getAllPostsApi(page) {
+  try {
+    const startIndex = (page - 1) * PER_PAGE;
+    const posts = await prisma.posts.findMany({
+      include: { categories: true },
+      skip: startIndex,
+      take: PER_PAGE
+    });
+
+    console.log('All Posts:', posts);
+    return posts;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
 }
